b0f3d32f0783aac6c57bebb983b967ad4304053f
Jeiser Medrano Abreu <jeiser.medrano@factory.generalsoftwareinc.com>
2025-11-26 20:59:15 -0300

    ultima refactorizacion

--- src/test/java/com/clasenoviembre/AllSuite.java
+++ src/test/java/com/clasenoviembre/AllSuite.java
@@ -3,12 +3,14 @@ package com.clasenoviembre;
 import org.junit.platform.suite.api.ExcludeTags;
 import org.junit.platform.suite.api.IncludeTags;
 import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectClasses({AppTest.class})
-@IncludeTags("area")
-@ExcludeTags("integration")
+//@SelectClasses({AppTest.class})   // Se puede usar SelectClasses para definir clases de test específicas
+@SelectPackages("com.clasenoviembre")  // Se puede usar SelectPackages para definir un paquete completo de test
+@IncludeTags("integration")
+@ExcludeTags("area")
 public class AllSuite {
     //NO SE DEFINEN NI ATRIBUTOS NI METODOS
 }
